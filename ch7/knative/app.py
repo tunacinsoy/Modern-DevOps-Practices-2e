1a9e1664927d3edc7ed6e6bfa580e12211abb202
Tuna Cinsoy <tunacinsoy@gmail.com>
2024-07-12 22:52:36 +0300

    fix: renamed the folder to ch8-terraform for better understandability

--- ch7/knative/app.py
+++ ch7/knative/app.py
@@ -1,10 +1,14 @@
 import os
 import datetime
 from flask import Flask
+
+# Print statement will also appear in console, when we set this value to 1
+os.environ['PYTHONUNBUFFERED'] = '1'
+
 app = Flask(__name__)
+
 @app.route('/')
 def current_time():
   ct = datetime.datetime.now()
+  print('Output is getting generated...')
   return 'The current time is : {}!\n'.format(ct)
-if __name__ == "__main__":
-  app.run(debug=True,host='0.0.0.0')
